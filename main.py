79d9820725c5417258bed2edfd6e87a841e42fc2
Xotlone <Xotlone@gmail.com>
2025-02-22 06:00:23 +0200

    Добавлена система локализации

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ from utils import constants
 
 load_dotenv('.env')
 
+localization_provider = disnake.LocalizationStore(strict=True)
+localization_provider.load('localization')
 intents = disnake.Intents.all()
 bot = commands.Bot(
     command_prefix='/',
     help_command=None,
     owner_ids=constants.OWNER_IDS,
     intents=intents,
+    localization_provider=localization_provider
 )
 
 for cog_name in os.listdir('./cogs'):
