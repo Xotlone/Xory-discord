76aadf598cdc0ee6220f09d6936c35c7cf2b78cd
Xotlone <Xotlone@gmail.com>
2025-02-22 06:15:37 +0200

    Добавлена команда dice, бросок игральной кости

--- cogs/dnd.py
+++ cogs/dnd.py
@@ -1,13 +1,49 @@
+from random import randint
+
 import disnake
+from disnake import Option, OptionType
+from disnake.i18n import Localized
 from disnake.ext import commands as dis_commands
 
 class Dnd(dis_commands.Cog):
     def __init__(self, bot: dis_commands.Bot):
         self.bot = bot
 
-    @dis_commands.slash_command(name='hello_world')
-    async def hello_world(self, inter: disnake.CommandInteraction):
-        await inter.send(content="Hello world!", ephemeral=True)
+    @dis_commands.slash_command(
+        name='dice',
+        description=Localized(key='dice.desc'),
+        options=[
+            Option(
+                name='sides',
+                description=Localized(key='dice.option1.desc'),
+                type=OptionType.integer,
+                min_value=2
+            ),
+            Option(
+                name='hidden',
+                description=Localized(key='dice.option2.desc'),
+                type=OptionType.boolean
+            )
+        ]
+    )
+    async def dice(self, inter: disnake.CommandInteraction, sides: int = 20,
+                   hidden: bool = False):
+        value = randint(1, sides)
+
+        color = disnake.Color.dark_embed()
+        if value == 1: color = disnake.Color.red()
+        elif value == 20: color = disnake.Color.gold()
+        if (value == 1) or (value == 20): value = f'**{value}**'
+
+        embed = disnake.Embed(
+            title=value,
+            timestamp=inter.created_at,
+            color=color
+        )
+        embed.set_author(name=inter.author.display_name,
+                         icon_url=inter.author.display_avatar.url)
+        await inter.send(embed=embed, ephemeral=hidden)
+
 
 def setup(bot: dis_commands.Bot):
     bot.add_cog(Dnd(bot))
